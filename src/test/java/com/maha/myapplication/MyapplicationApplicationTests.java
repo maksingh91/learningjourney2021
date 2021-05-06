b52efc0b0212fa487505a6ab6d3e6d75198cf2c7
maksingh91 <maksingh91@gmail.com>
2021-05-06 09:26:12 +0530

    first ever commit

---
+++ src/test/java/com/maha/myapplication/MyapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maha.myapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
